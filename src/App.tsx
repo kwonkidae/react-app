a457d2bb5d415a7d5c1bcefde80d5be28db7dc7a
kwon <kkdosk@naver.com>
2018-07-26 15:57:48 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import './App.sass';
+
+// import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Lawtalk News Admin</h1>
+        </header>
+        <Editor />
+      </div>
+    );
+  }
+}
+
+export default App;
