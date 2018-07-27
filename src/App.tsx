ac640c0b16240cb4b5fcd77c100104c61e7ce459
kwon <kkdosk@naver.com>
2018-07-27 18:59:32 +0900

    haha

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Lawtalk News Admin Chagne</h1>
+          <h1 className="App-title">Lawtalk News Admin hahahaha</h1>
         </header>
         <Editor />
       </div>
